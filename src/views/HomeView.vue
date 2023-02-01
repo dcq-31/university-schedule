3a754fe3dcef98ad80abafe819abe6ae2dbf3175
dcq <dariancapoteq@gmail.com>
2023-02-01 18:00:59 -0500

    Remove unnecessary default project code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="font-sans text-2xl p-10">Home View</div>
   </main>
 </template>
