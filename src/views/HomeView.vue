2aa28d406c25f78667648f092f5b57c8d005f2a2
dcq <dariancapoteq@gmail.com>
2023-02-03 16:15:33 -0500

    Improve home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,28 @@
 <script lang="ts" setup>
 import { RouterLink } from "vue-router";
 import { ROUTES_NAME } from "../router/names";
+import { CalendarDaysIcon } from "@heroicons/vue/24/outline";
 </script>
 
 <template>
-  <div class="p-1">
+  <div class="p-2">
     <main class="py-2 px-3">
-      <div class="p-3 text-sm rounded-md border border-slate-200">
-        <div class="font-semibold text-slate-700">Eres profesor?</div>
-        <div class="text-slate-500">
-          Ingresa tu cuenta de usario para tener acceso a la administración
-        </div>
+      <div
+        class="p-3 border border-brandeis-blue-100 text-sm rounded-md flex items-center space-x-2"
+      >
+        <div>
+          <div class="font-semibold text-slate-600">¿Eres profesor?</div>
+          <div class="text-slate-500 mt-2">
+            Ingresa tu cuenta de usuario para tener acceso a la administración
+          </div>
 
-        <RouterLink
-          :to="{ name: ROUTES_NAME.LOGIN }"
-          class="block w-min ml-auto mr-5 px-3 py-1.5 text-xs tracking-wider font-medium rounded-lg text-brandeis-blue text-center bg-slate-100 hover:shadow-sm hover:bg-slate-200 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-200 focus:ring-offset-2"
-        >
-          Visitar
-        </RouterLink>
+          <RouterLink
+            :to="{ name: ROUTES_NAME.LOGIN }"
+            class="block mt-2 font-medium text-brandeis-blue hover:text-brandeis-blue-500"
+          >
+            Inicia sesión
+          </RouterLink>
+        </div>
       </div>
 
       <div class="mt-5">
@@ -25,27 +30,28 @@ import { ROUTES_NAME } from "../router/names";
 
         <div class="mt-3">
           <RouterLink :to="{ name: ROUTES_NAME.LOGIN }">
-            <div
-              class="flex items-center space-x-3 p-3 rounded-lg border border-transparent hover:border-brandeis-blue-100"
-            >
-              <img
-                src="/icons/calendar.png"
-                alt="calendar icon"
-                class="h-16 w-16"
-              />
-
-              <div>
-                <div class="text-slate-700 text-sm font-semibold">
+            <div class="py-2 px-1 w-fit rounded-lg flex items-center space-x-3">
+              <div class="shrink-0">
+                <CalendarDaysIcon class="text-brandeis-blue-500 h-16 w-16" />
+              </div>
+              <div class="space-y-1">
+                <div class="text-slate-600 text-sm font-semibold">
                   Horario de Clases
                 </div>
-                <div class="text-slate-500 mt-1 text-xs tracking-wide">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                <div class="text-slate-500 text-xs tracking-wider">
+                  ¿No sabes que clases tocan hoy o cuando tienes exámen? Revisa
+                  nuestro calendario.
                 </div>
-                <div>
+                <div class="ml-2 space-x-1">
                   <span
-                    class="text-carrot-orange font-medium text-xs py-1 px-2 rounded-xl bg-orange-50"
+                    class="text-carrot-orange text-xs py-1 px-2 rounded-xl bg-orange-50"
                   >
                     Estudiante
+                  </span>
+                  <span
+                    class="text-brandeis-blue text-xs py-1 px-2 rounded-xl bg-blue-50"
+                  >
+                    Profesor
                   </span>
                 </div>
               </div>
