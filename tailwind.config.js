5c1c94de7615a4b2367b565bcb98702b3e28fac4
dcq <dariancapoteq@gmail.com>
2023-03-11 22:38:56 -0500

    Override tailwind screen breakpoints based on Bootstrap

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,13 @@ module.exports = {
           900: "#452e0a",
         },
       },
+      screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1400px",
+      },
     },
   },
   plugins: [],
